8cb2ef2db26ba1f4417b27d01e332caefa425a51
meksula <meksula.karol.93@gmail.com>
2021-08-02 22:58:43 +0200

    Spring Boot Gateway project for format-transformer added

---
+++ src/test/java/io/meksula/gateway/FormatTransformerGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.meksula.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormatTransformerGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
